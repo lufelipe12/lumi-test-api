5cb656a6c518d728d50c0e1c79c3ea57a4e3cdc5
Luiz Felipe Dias <lufelipe.dias@gmail.com>
2023-12-22 00:32:18 -0300

    feat: added new redis module on app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 
-import { PrismaModule } from './infra/database/prisma/prisma.module';
+import { PrismaModule } from '@infra/database/prisma/prisma.module';
+import { RedisModule } from '@infra/cache/redis/redis.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), PrismaModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    PrismaModule,
+    RedisModule,
+  ],
 })
 export class AppModule {}
