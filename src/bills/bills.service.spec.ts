67020a1cca4d43de6867d1f971544b0f96679be5
Luiz Felipe Dias <lufelipe.dias@gmail.com>
2023-12-24 11:02:24 -0300

    test: added test for should be defined

--- src/bills/bills.service.spec.ts
+++ src/bills/bills.service.spec.ts
@@ -1,12 +1,14 @@
 import { Test, TestingModule } from '@nestjs/testing';
+
 import { BillsService } from './bills.service';
+import { cacheManagerMock, prismaServiceMock } from '../testing';
 
 describe('BillsService', () => {
   let service: BillsService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [BillsService],
+      providers: [BillsService, cacheManagerMock, prismaServiceMock],
     }).compile();
 
     service = module.get<BillsService>(BillsService);
