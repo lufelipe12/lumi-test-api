841d27929bea7a37460c072df9b17940b167d4c7
Luiz Felipe Dias <lufelipe.dias@gmail.com>
2023-12-24 11:14:22 -0300

    test: added testing for read methods on bills service

--- src/bills/bills.service.spec.ts
+++ src/bills/bills.service.spec.ts
@@ -1,20 +1,39 @@
 import { Test, TestingModule } from '@nestjs/testing';
 
 import { BillsService } from './bills.service';
-import { cacheManagerMock, prismaServiceMock } from '../testing';
+import {
+  billMock,
+  billsPaginatedMock,
+  cacheManagerMock,
+  prismaServiceMock,
+} from '../testing';
 
 describe('BillsService', () => {
-  let service: BillsService;
+  let billsService: BillsService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [BillsService, cacheManagerMock, prismaServiceMock],
     }).compile();
 
-    service = module.get<BillsService>(BillsService);
+    billsService = module.get<BillsService>(BillsService);
   });
 
   it('should be defined', () => {
-    expect(service).toBeDefined();
+    expect(billsService).toBeDefined();
+  });
+
+  describe('Read', () => {
+    it('Should find all bills paginated', async () => {
+      const result = await billsService.findAll(1, 5);
+
+      expect(result).toEqual(billsPaginatedMock);
+    });
+
+    it('Should return a bill by id', async () => {
+      const result = await billsService.findOne(1);
+
+      expect(result).toEqual(billMock);
+    });
   });
 });
